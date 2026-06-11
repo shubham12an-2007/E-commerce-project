87df5fb625b88ec6bd81c86a702cef21164d73a0
Shubham Kumar Singh <shubham12an@gmail.com>
2026-06-11 17:33:58 +0530

    added image of each product in our project

--- src/main/java/com/shubham/ecom_project/controller/ProductController.java
+++ src/main/java/com/shubham/ecom_project/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.shubham.ecom_project.controller;
 import com.shubham.ecom_project.model.Product;
 import com.shubham.ecom_project.service.ProductService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,48 +15,58 @@ import java.util.List;
 @RequestMapping("/api")
 public class ProductController {
 
-    private final ProductService productService ;
+    private final ProductService productService;
 
-    public ProductController(ProductService productService){
-        this.productService = productService ;
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
 
 
     @GetMapping("/products")
-    public ResponseEntity<List<Product>> getAllProducts(){
+    public ResponseEntity<List<Product>> getAllProducts() {
 
-        return new ResponseEntity<>(productService.getAllProducts() , HttpStatus.OK) ;
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 
     @GetMapping("/product/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable int id ){
+    public ResponseEntity<Product> getProduct(@PathVariable int id) {
 
-        Product product = productService.getProduct(id) ;
+        Product product = productService.getProduct(id);
 
-        if(product != null){
-            return new ResponseEntity<>(product , HttpStatus.OK)  ;
-        }
-        else {
-           return   new ResponseEntity<>(HttpStatus.NOT_FOUND) ;
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
 
     }
 
     @PostMapping("/product")
-    public ResponseEntity<?> addProduct(@RequestPart Product product ,
-                                        @RequestPart MultipartFile imageFile){
+    public ResponseEntity<?> addProduct(@RequestPart Product product,
+                                        @RequestPart MultipartFile imageFile) {
 
         try {
-            Product product1 = productService.addProduct(product , imageFile) ;
-            return  new ResponseEntity<>(product1 , HttpStatus.OK) ;
-        }
-        catch (Exception e){
-            return new ResponseEntity<>(e.getMessage() , HttpStatus.INTERNAL_SERVER_ERROR) ;
+            Product product1 = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(product1, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
-
 
 
     }
+
+
+    @GetMapping("/product/{prodId}/image")
+    public ResponseEntity<byte[]> getImageProductById(@PathVariable int prodId) {
+
+        Product product = productService.getProduct(prodId);
+        byte[] imageFile = product.getImageData();
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(product.getImageType()))
+                .body(imageFile) ;
+    }
+
 }
